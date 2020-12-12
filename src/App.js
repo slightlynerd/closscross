3ea5007002cc8e3e2864cc9e0fcefd933fb94bff
Kelvin Gobo <gobokel@gmail.com>
2020-12-12 20:48:35 +0100

    setup boilerplate, add header, navbar compts

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import Header from './components/Header';
 import logo from './logo.svg';
 import './App.css';
 
@@ -5,6 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Header />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
