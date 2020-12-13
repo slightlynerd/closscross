2a0f49341c54773880fe9122ae3786cab7208dc5
Kelvin Gobo <gobokel@gmail.com>
2020-12-13 17:13:36 +0100

    feat(navbar): implement navbar design

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,157 +1,95 @@
+import { useState } from "react";
+import cn from "classnames";
 import logo from "../assets/logo.svg";
+import graduating from "../assets/graduating.svg";
+import search from "../assets/search.svg";
+import star from "../assets/star.svg";
+import "../styles/Navbar.css";
 
 export default function Navbar() {
+  const [mode, setMode] = useState("training");
+
   return (
-    <nav className="bg-gray-800">
-      <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
-        <div className="relative flex items-center justify-between h-16">
-          <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+    <nav className="navbar px-8 py-3">
+      <div className="flex justify-between">
+        {/* logo */}
+        <div className="flex items-center justify-center">
+          <div className="flex items-center mr-4">
+            <img className="block h-8 w-auto" src={logo} alt="logo"></img>
+          </div>
+          <div className="border border-gray-500 text-white text-sm rounded-sm">
             <button
-              className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
-              aria-expanded="false"
+              className={cn({
+                "bg-white text-black": mode === "training",
+                "px-3 font-semibold py-1": true,
+              })}
+              onClick={() => setMode("training")}
             >
-              <span className="sr-only">Open main menu</span>
-              <svg
-                className="block h-6 w-6"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                aria-hidden="true"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M4 6h16M4 12h16M4 18h16"
-                />
-              </svg>
-              <svg
-                className="hidden h-6 w-6"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                aria-hidden="true"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M6 18L18 6M6 6l12 12"
-                />
-              </svg>
+              Training Mode
             </button>
-          </div>
-          <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
-            <div className="flex-shrink-0 flex items-center">
-              <img
-                className="block lg:hidden h-8 w-auto"
-                src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
-                alt="Workflow"
-              ></img>
-              <img
-                className="hidden lg:block h-8 w-auto"
-                src={logo}
-                alt="Workflow"
-              ></img>
-            </div>
-            <div className="hidden sm:block sm:ml-6">
-              <div className="flex space-x-4">
-                <a
-                  href="#"
-                  className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium"
-                >
-                  Dashboard
-                </a>
-                <a
-                  href="#"
-                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-                >
-                  Team
-                </a>
-                <a
-                  href="#"
-                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-                >
-                  Projects
-                </a>
-                <a
-                  href="#"
-                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-                >
-                  Calendar
-                </a>
-              </div>
-            </div>
-          </div>
-          <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-            <button className="bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
-              <span className="sr-only">View notifications</span>
-
-              <svg
-                className="h-6 w-6"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                aria-hidden="true"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
-                />
-              </svg>
+            <button
+              className={cn({
+                "bg-white text-black": mode === "live",
+                "px-3 font-semibold py-1": true,
+              })}
+              onClick={() => setMode("live")}
+            >
+              Live Mode
             </button>
-
-            <div className="ml-3 relative">
-              <div>
-                <button
-                  className="bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
-                  id="user-menu"
-                  aria-haspopup="true"
-                >
-                  <span className="sr-only">Open user menu</span>
-                  <img
-                    className="h-8 w-8 rounded-full"
-                    src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                    alt=""
-                  ></img>
-                </button>
-              </div>
-            </div>
           </div>
         </div>
-      </div>
-
-      <div className="hidden sm:hidden">
-        <div className="px-2 pt-2 pb-3 space-y-1">
-          <a
-            href="#"
-            className="bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium"
-          >
-            Dashboard
-          </a>
-          <a
-            href="#"
-            className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
-          >
-            Team
-          </a>
-          <a
-            href="#"
-            className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
-          >
-            Projects
-          </a>
-          <a
-            href="#"
-            className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
-          >
-            Calendar
-          </a>
+        {/* filter */}
+        <div className="flex items-center justify-between text-white">
+          <div>
+            <button className="border border-gray-500 rounded-sm font-semibold py-1 px-3 text-sm mr-2">
+              Show All
+            </button>
+          </div>
+          <div>
+            <button className="border border-gray-500 bg-white text-black rounded-sm font-semibold py-1 px-3 text-sm mr-2">
+              Crypto
+            </button>
+          </div>
+          <div>
+            <button className="border border-gray-500 rounded-sm font-semibold py-1 px-3 text-sm mr-2">
+              Commodities
+            </button>
+          </div>
+          <div>
+            <button className="border border-gray-500 bg-white text-black rounded-sm font-semibold py-1 px-3 text-sm mr-2">
+              Stock
+            </button>
+          </div>
+          <div>
+            <button className="border border-gray-500 rounded-sm font-semibold py-1 px-3 text-sm mr-2">
+              Index
+            </button>
+          </div>
+          <div>
+            <button className="border border-gray-500 rounded-sm font-semibold py-1 px-3 text-sm mr-2">
+              Currency
+            </button>
+          </div>
+        </div>
+        {/* notification */}
+        <div className="ml-3 flex relative">
+          <img src={search} alt="search" className="mr-6"></img>
+          <img src={star} alt="star" className="mr-6"></img>
+          <img src={graduating} alt="graduating" className="mr-6"></img>
+          <div>
+            <button
+              className="bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+              id="user-menu"
+              aria-haspopup="true"
+            >
+              <span className="sr-only">Open user menu</span>
+              <img
+                className="h-8 w-8 rounded-full"
+                src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                alt=""
+              ></img>
+            </button>
+          </div>
         </div>
       </div>
     </nav>
